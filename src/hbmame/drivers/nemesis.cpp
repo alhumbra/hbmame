bb835f842a63f185d20e6b624401a2448c6ea65e
alhumbra <164629245+alhumbra@users.noreply.github.com>
2025-04-08 02:18:45 +0900

    Update nemesis.cpp SPclone DIP
    Changed from LifeforceJ to SPclone to allow the use of dip switches, a feature added in Salamander (version YY clone).

--- src/hbmame/drivers/nemesis.cpp
+++ src/hbmame/drivers/nemesis.cpp
@@ -2,7 +2,7 @@
 // copyright-holders:Robbbert
 #include "../mame/drivers/nemesis.cpp"
 
-#if 0
+
 static INPUT_PORTS_START( spclone )
 	PORT_START("IN0")
 	PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_COIN1 )
@@ -130,6 +130,6 @@ ROM_START( spcloneo )
 	ROM_LOAD( "587-c01.10a",   0x00000, 0x20000, CRC(09fe0632) SHA1(4c3b29c623d70bbe8a938a0beb4638912c46fb6a) )
 ROM_END
 
-GAME( 2007, spclone,  salamand, salamand, lifefrcj, nemesis_state, empty_init, ROT0, "Konami", "Salamander (version YY 0.7.3) ", MACHINE_SUPPORTS_SAVE )
-GAME( 2007, spcloneo, salamand, salamand, lifefrcj, nemesis_state, empty_init, ROT0, "Konami", "Salamander (version YY 0.7.2.2)", MACHINE_SUPPORTS_SAVE )
+GAME( 2007, spclone,  salamand, salamand, spclone, nemesis_state, empty_init, ROT0, "Konami", "Salamander (version YY 0.7.3) ", MACHINE_SUPPORTS_SAVE )
+GAME( 2007, spcloneo, salamand, salamand, spclone, nemesis_state, empty_init, ROT0, "Konami", "Salamander (version YY 0.7.2.2)", MACHINE_SUPPORTS_SAVE )
 
